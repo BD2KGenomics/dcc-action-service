0e6271f0fe597549a9e6bca8a3f2568314ad833e
Brian O'Connor <briandoconnor@gmail.com>
2016-12-29 17:22:47 -0800

    updates

--- luigi_task_executor/AlignmentQCTaskV2.py
+++ luigi_task_executor/AlignmentQCTaskV2.py
@@ -103,12 +103,11 @@ class ConsonanceTaskV2(luigi.Task):
         #This helps make the task idempotent; it that it
         #always has the same task id for the same inputs
         task_uuid = uuid5(uuid.NAMESPACE_DNS, self.filename +
-        str(self.file_uuid) +
+        self.file_uuid +
         self.target_tool +
         self.target_tool_url +
         self.redwood_token +
-        self.redwood_host +
-        str(''.join(map("'{0}'".format, self.parent_uuids))))
+        self.redwood_host)
         return task_uuid
 
 class AlignmentQCCoordinatorV2(luigi.Task):
